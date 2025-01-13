62b4826fe08f822ee24b7608eaf30c0e7e1a9cd1
isabelfalin <fal24007@byui.edu>
2025-01-13 15:28:01 -0700

    dark mission theme done

--- mission/mission.js
+++ mission/mission.js
@@ -1,1 +1,14 @@
-const selectElement = document.getElementById("menu"); 
+const themeSelector = document.getElementById("menu");
+const logoimage = document.getElementById("logo");
+function changeTheme() {
+    
+if (themeSelector.value == "dark") {
+    logoimage.setAttribute("src", "byui-logo_white.png");
+    document.body.classList.add("dark");
+} else {
+    logoimage.setAttribute("src", "byui-logo_blue.webp");
+    document.body.classList.remove("dark");
+}
+}
+
+themeSelector.addEventListener('change', changeTheme);
