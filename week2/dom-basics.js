67c8278dfe32493e628fc027729b0149d4613f73
isabelfalin <fal24007@byui.edu>
2025-01-14 13:46:38 -0700

    dom assignment done

--- week2/dom-basics.js
+++ week2/dom-basics.js
@@ -5,4 +5,12 @@ document.body.appendChild(newParagraph);
 const image = document.createElement("img")
 image.setAttribute("src", "https://picsum.photos/200")
 image.setAttribute("alt", "Picsum Image")
-document.body.appendChild(image)
+document.body.appendChild(image);
+
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+
+const section = document.createElement("section")
+section.innerHTML = ("<h2>DOM Basics</h2><p>This was added through Javascript</p>")
+document.body.appendChild(section);
