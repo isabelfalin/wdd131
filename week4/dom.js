39e4b01678f6d8eb08197e97fc58fb4da20056f2
isabelfalin <fal24007@byui.edu>
2025-01-27 21:17:49 -0700

    i am so done with DOM

--- week4/dom.js
+++ week4/dom.js
@@ -3,19 +3,37 @@ let tasks = [];
 
 function renderTasks(tasks) {
   // get the list element from the DOM
+  const thelist = document.getElementById("todoList");
+
   // loop through the tasks array. transform (map) each task object into the appropriate HTML to represent a to-do.
-  { detail: task, completed; false}
+  let allthelistitems = "";
+  
+  tasks.forEach(task => {
+      allthelistitems += `<li ${task.completed ? 'class="strike"' : ""}>
+    <p>${task.detail}</p>
+    <div>
+      <span data-function="delete">❎</span>
+      <span data-function="complete">✅</span>
+    </div>
+  </li>`;
+
+  });
+  thelist.innerHTML = allthelistitems;
 }
 
-function newTask(todo) {
+function newTask() {
   // get the value entered into the #todo input
+  const todo = document.getElementById("todo");
+
   // add it to our arrays tasks
+  let thetask = { detail: todo.value, completed: false}
+  tasks.push(thetask);
+
   // render out the list
-  { detail: task, completed; false}
+  renderTasks(tasks);
 }
 
 function removeTask(taskElement) {
-    { detail: task, completed false}
   // Note the use of Array.filter to remove the element from our task array
   // Notice also how we are using taskElement instead of document as our starting point?
   // This will restrict our search to the element instead of searching the whole document.
@@ -28,10 +46,10 @@ function removeTask(taskElement) {
 }
 
 function completeTask(taskElement) {
-    { detail: task, completed; false}
+  console.log(taskElement)
   // In this case we need to find the index of the task so we can modify it.
   const taskIndex = tasks.findIndex(
-    (task) => task.detail === taskElement.childNodes[0].innerText
+    (task) => task.detail === taskElement.querySelector('p').innerText
   );
   // once we have the index we can modify the complete field.
   // tasks[taskIndex].completed ? false : true is a ternary expression.
@@ -43,10 +61,16 @@ function completeTask(taskElement) {
 }
 
 function manageTasks(event) {
-    { detail: task, completed; false}
   // did they click the delete or complete icon?
-  console.log(event.target);
-  console.log(event.currentTarget);
+  const theaction = event.target.dataset.function;
+  if(theaction == "delete")
+  {
+    removeTask(event.target.closest("li"));
+  }
+  else if(theaction == "complete")
+  {
+    completeTask(event.target.closest("li"));
+  }
   // event.target will point to the actual icon clicked on. We need to get the parent li to work with however. HINT: Remember element.closest()? Look it up if you don't
 
   // because we added 'data-action="delete"' to each icon in a task we can access a dataset property on our target (e.target.dataset.action)
@@ -55,3 +79,9 @@ function manageTasks(event) {
 
 // Add your event listeners here
 // We need to attach listeners to the submit button and the list. Listen for a click, call the 'newTask' function on submit and call the 'manageTasks' function if either of the icons are clicked in the list of tasks.
+
+const submitTask = document.getElementById("submitTask");
+submitTask.addEventListener("click", newTask);
+
+const todoList = document.getElementById("todoList");
+todoList.addEventListener("click", manageTasks);
