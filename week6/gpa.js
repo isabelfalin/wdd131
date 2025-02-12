b9e548988776a84fd6e30c21fcad8c86449cbfa0
isabelfalin <fal24007@byui.edu>
2025-02-12 12:39:36 -0700

    java scipt functions assignemnt

--- week6/gpa.js
+++ week6/gpa.js
@@ -1,12 +1,69 @@
-function getGrades(){
+function getGrades() {
     const gradesInput = document.getElementById("grades"); 
     const gradesString = gradesInput.value;
-    console.log(gradesString);
+
+
+    const gradeList = gradesString.split(",")
+
+    gradeList.forEach((item, index) => {
+        gradeList[index] = item.trim().toUpperCase();
+
+    });
+
+    return gradeList;
 
 }
 
+function convertGrade(grade) {
+    if (grade == "A") {
+        return 4;
+    }
+    if (grade == "B") {
+        return 3;
+    }
+    if (grade == "C") {
+        return 2;
+    }
+    if (grade == "D") {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+function calculateAverage(gradeList){
+
+    let numberList = [];
+    gradeList.forEach((item, index) => {
+        const theNumber = convertGrade(item);
+        
+        numberList.push(theNumber);
+    });
+
+    let total = 0;
+    numberList.forEach((number, index) => {
+        total += number;
+    });
+
+
+    const average = total / numberList.length;
+    return average;
+}
+
+function displayGpa(gpa){
+    const outputElement = document.getElementById("output");
+    outputElement.textContent = gpa.toFixed(2);
+}
+
+
+
+
+
 function calculateGrade(){
-    getGrades();
+    const gradeList = getGrades();
+    const theAverage = calculateAverage(gradeList);
+    displayGpa(theAverage);
 }
 
 const button = document.getElementById("submitButton"); 
